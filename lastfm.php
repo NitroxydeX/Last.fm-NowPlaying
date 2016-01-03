9b83b641e86d46305a7fb95615f5d1e3d3c93577
Tobias <tobias.reinhard@me.com>
2016-01-03 23:29:09 +0100

    update to v1.0.3

--- lastfm.php
+++ lastfm.php
@@ -1,15 +1,4 @@
 <script type="text/javascript">
-// Change your username here
-username = "txbi";
-// ---------------------- //
-$.get("//ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + username + "&api_key=1f633977acf0e2d0630ec11dbc350d3e&format=json", function(data) {
-    if (typeof data.recenttracks.track[0]["@attr"] != "undefined") {
-        artist = data.recenttracks.track[0].artist["#text"];
-        track = data.recenttracks.track[0].name;
-        album = data.recenttracks.track[0].album["#text"];
-        $("#main").html("<p class='track'>" + track + "</p>" + "<p class='artist'>" + artist + "</p>" + "<p class='album'>" + album + "</p>");
-    } else {
-    	$("#main").html("<p class='track'>Scrobblen nicht möglich ...</p>"+"<p class='artist'>Gerade wird keine Musik gestreamt.</p>");
-    }
-});
+// change your username here first
+username="txbi",$.get("//ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+username+"&api_key=1f633977acf0e2d0630ec11dbc350d3e&format=json",function(t){"undefined"!=typeof t.recenttracks.track[0]["@attr"]?(artist=t.recenttracks.track[0].artist["#text"],track=t.recenttracks.track[0].name,album=t.recenttracks.track[0].album["#text"],$("#main").html("<p class='track'>"+track+"</p><p class='artist'>"+artist+"</p><p class='album'>"+album+"</p><p class='now'><i class='fa fa-music'></i>Scrobbelt gerade!</p>")):$("#main").html("<p class='track'>Scrobblen nicht möglich ...</p><p class='artist'>Gerade wird keine Musik gestreamt.</p>")});
 </script>
